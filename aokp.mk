da1111748ab2e308af8eb3a3cd6f8a38f0d0e964
erikcas <erikcas1972@gmail.com>
2013-03-02 22:13:00 +0100

    initial jb-mr1 branch

--- aokp.mk
+++ aokp.mk
@@ -1,20 +1,19 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+pecify phone tech before including full_phone
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/tiny.mk)
+$(call inherit-product, vendor/aokp/configs/mini.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/tass/full_tass.mk)
+$(call inherit-product, device/samsung/tass/full_beni.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyTass
 PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5570
-#-include vendor/cm/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tass
-PRODUCT_NAME := cm_tass
+PRODUCT_NAME := aokp_tass
 
 ## Bootanimation
 TARGET_BOOTANIMATION_NAME := vertical-240x320
