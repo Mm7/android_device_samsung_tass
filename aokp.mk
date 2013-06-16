83069dc309f1ffa6069ebcfea7d50bd2a105c380
erikcas <erikcas1972@gmail.com>
2013-06-16 02:11:38 +0200

    Fix tass product inheritance (Androidmeda)

--- aokp.mk
+++ aokp.mk
@@ -1,11 +1,12 @@
 pecify phone tech before including full_phone
 $(call inherit-product, vendor/aokp/configs/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/aokp/configs/mini.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/samsung/tass/full_beni.mk)
+$(call inherit-product, device/samsung/tass/full_tass.mk)
+
+TARGET_SCREEN_HEIGHT := 320
+TARGET_SCREEN_WIDTH := 240
+$(call inherit-product, vendor/aokp/configs/mini.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyTass
