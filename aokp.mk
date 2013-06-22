1f8784548ac6ba465381b7a99add757c9123706c
erikcas <erikcas1972@gmail.com>
2013-06-22 16:32:50 +0200

    aokp

--- aokp.mk
+++ aokp.mk
@@ -4,11 +4,11 @@ TARGET_BOOTANIMATION_NAME := vertical-240x320
 # Inherit device configuration for Tass
 $(call inherit-product, device/samsung/tass/full_tass.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
 # Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 
 # Release name and versioning
@@ -17,4 +17,4 @@ PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5570
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tass
-PRODUCT_NAME := cm_tass
+PRODUCT_NAME := aokp_tass
