4099c6241315895f3679b7e4d02f3518ac2b63d4
erikcas <erikcas1972@gmail.com>
2013-06-22 19:24:24 +0200

    aokp
    Conflicts:
    	vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-
-add_lunch_combo aokp_tass-userdebug
+add_lunch_combo full_tass-userdebug
